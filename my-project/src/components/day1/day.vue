267093d38b758780a0aa3698c98006c9d3ced4fa
YuyaoYan <yanyy2@yonyou.com>
2018-08-13 15:32:47 +0800

    整理并删除部分页面

---
+++ my-project/src/components/day1/day.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <sub-vue></sub-vue>
+    </div>
+</template>
+<script>
+    import SubVue from './sub.vue';
+    export default {
+        filters: {
+            myFilter: funtion(value) {  //value就是text
+                //输入的内容进行反转
+                //转换成数组->翻转数组->转换成字符串
+                return value.split('').reverse().join('');
+            }
+        },
+        data() {
+            return {
+                text: ''
+            }
+        },
+        components: {
+          subVue: SubVue
+        }
+    }
+</script>
---
+++ my-project/src/components/day1/sub.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{'大家好，我是sub | myFilter'}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
